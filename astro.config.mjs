f1056418f4a42be2967dead9a175517148192f7a
Youssef Khaled <youssefkhaled3301@gmail.com>
2025-10-02 15:02:29 +0300

    Revert "feat: integrate Netlify adapter for deployment in Astro configuration"
    This reverts commit a7c62d201c813c1b2c8e4535c77032eae12ecea3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: netlify(),
 });
