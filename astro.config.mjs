ec43d8e2dc47050f5026478a511f7c0be2f66fad
Youssef Khaled <youssefkhaled3301@gmail.com>
2025-10-04 17:02:12 +0300

    feat: update sitemap integration with change frequency and enhance BaseLayout with canonical and Open Graph meta tags for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,9 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   site: "https://youssef-khaled.netlify.app",
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      changefreq: "weekly",
+    }),
+  ],
 });
